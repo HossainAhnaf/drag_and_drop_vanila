cc392168cb009d4c1d89796baf81dcd05958c950
Your Name <you@example.com>
2024-04-18 16:52:24 +0600

    backup

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
-class useHorizontalDragableList {
+class UseHorizontalDragableList {
   constructor(element) {
-    this.listContainer = element;   
-    this.init();
+
+    this.listContainer = element;
+    this.dragStartEventName = this.isTouchDevice ? "touchstart" : "mousedown"
+    this.dragMoveHandlerName = this.isTouchDevice ? "touchmove" : "mousemove"
+    this.dragEndEventName = this.isTouchDevice ? "touchend" : "mouseup"
+    
+    
+    this.isTouchDevice = ('ontouchstart' in window) ||
+    (navigator.maxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0);
+
+    this.touchDeviceScanInterval = setInterval(() => {
+      this.isTouchDevice = ('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0);
+      }, 3000)
+    this.init()
   }
-  init(){
+  init() {
     Array.from(this.listContainer.children).forEach((item) => {
-    item.setAttribute("draggable", "true");
-    item.addEventListener("dragstart", (e) => {
-      item.classList.add("dragging");
-    });
+      item.addEventListener(this.dragStartEventName, this.#dragStartHandler.bind(this))
+      item.addEventListener(this.dragEndEventName, this.#dragEndHandler.bind(this))
+    })
+  }
 
-    
-    item.addEventListener("dragend", (e) => {
-      item.classList.remove("dragging");
-    });
-        
-    item.addEventListener("dragover", (e) => {
-      if (!item.classList.contains("dragging")) {
-      item.classList.add("draggover");
-      }
-    });
-    item.addEventListener("dragleave", (e) => {
-        item.classList.remove("draggover");
-    });
-    
-  })
+  #dragStartHandler(e) {
+    console.log(this.isTouchDevice);
+
+    const startYOffset = this.isTouchDevice ? e.touches[0].clientY : e.clientY
+  
+    e.currentTarget.setAttribute("data-dragging", "true");
+    e.currentTarget.addEventListener(this.dragMoveHandlerName, (e) => this.#dragMoveHandler(e, startYOffset))
+  }
+  #dragMoveHandler(e, startYOffset) {
+    const clientY = this.isTouchDevice ? e.touches[0].clientY : e.clientY
+    if (e.currentTarget.getAttribute("data-dragging") == "true") {
+      e.currentTarget.style.top = clientY - startYOffset + "px";
+    }
 
   }
+  #dragEndHandler({ currentTarget }) {
+    currentTarget.setAttribute("data-dragging", "false");
+    currentTarget.removeEventListener("mousemove", this.#dragMoveHandler)
+  }
+
+
 }
