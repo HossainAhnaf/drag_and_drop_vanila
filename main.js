30f8b0a921dbd64f8d8e7297553d875c1bf3b3ca
Your Name <you@example.com>
2024-04-20 21:22:12 +0600

    implementing transition show

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ class HorizontalDragableList {
       item.style.setProperty("touch-action", "none");
       item.style.setProperty("z-index", "0")
       item.addEventListener("pointerdown", this.#dragStartHandler.bind(this))
+
     })
   }
   #updateMinMaxYOffset() {
@@ -66,13 +67,22 @@ class HorizontalDragableList {
     updateDraggingState.bind(this)()
     updateNextPrevState.bind(this)()
   }
-
+  #changeDraggingElementVisualIndex({ elm: dragOverElement, rect: dragOverElmRect}) {
+    const oldTransformValue = /\d+/.exec(dragOverElement.style.getPropertyValue("transform"))
+    const oldTranslateY = (oldTransformValue ? parseInt(oldTransformValue[0]) : 0)
+    const dragOverElementVisualIndex = parseInt(dragOverElement.getAttribute("data-visual-index")) 
+    dragOverElement.style.setProperty("transform", `translateY(${this.draggingState.startRect.top - dragOverElmRect.top - (this.draggingState.visualIndex - dragOverElementVisualIndex === 1 ? oldTranslateY : -oldTranslateY)}px)`)
+    dragOverElement.setAttribute("data-visual-index", this.draggingState.visualIndex)
+    this.draggingState.startRect = dragOverElmRect
+    this.#updateState(null, dragOverElementVisualIndex)
+  }
+  
   #dragStartHandler(e) {
     this.#updateState(e.currentTarget, parseInt(e.currentTarget.getAttribute("data-visual-index")))
     this.startYOffset = e.clientY
     this.draggingState.elm.setAttribute("data-dragging", "true");
     this.draggingState.elm.style.setProperty("z-index", "1")
-    document.addEventListener("pointermove",this.#dragMoveHandler.bind(this))
+    document.onpointermove = this.#dragMoveHandler.bind(this)
     document.addEventListener("pointerup", this.#dragEndHandler.bind(this))
     document.documentElement.style.setProperty("cursor", "grabbing")
     this.draggingState.elm.style.setProperty("cursor", "grabbing")
@@ -83,25 +93,37 @@ class HorizontalDragableList {
     const draggingElmRect = this.draggingState.elm.getBoundingClientRect()
     const currentTranslateY = Math.min(this.minYOffset,Math.max(e.clientY,this.maxYOffset)) - this.startYOffset 
     this.draggingState.elm.style.setProperty("transform", `translateY(${(currentTranslateY)}px)`)
-      const isPrevElementDraggedOver = (this.prevState.rect && draggingElmRect.top <= this.prevState.rect.top + this.prevState.rect.height / 2)
-      const isNextElementDraggedOver = (this.nextState.rect && draggingElmRect.top >= this.nextState.rect.top - this.nextState.rect.height / 2)
+    //check if the dragged element is dragged over the prev or next element
+    const isPrevElementDraggedOver = (this.prevState.rect && draggingElmRect.top <= this.prevState.rect.bottom)
+    const isNextElementDraggedOver = (this.nextState.rect && draggingElmRect.bottom >= this.nextState.rect.top)
+    if (isPrevElementDraggedOver || isNextElementDraggedOver)
+      this.#dragOverHandler(isPrevElementDraggedOver,draggingElmRect)
+  }
+}
 
-      if (isPrevElementDraggedOver || isNextElementDraggedOver)
-        this.#changeDraggingElementVisualIndex(isPrevElementDraggedOver ? this.prevState : this.nextState)
-  
+ #dragOverHandler(isPrevElementDraggedOver,draggingElmRect)  {
+  const { elm,rect } = (isPrevElementDraggedOver ? this.prevState : this.nextState )
+  elm.style.setProperty("transition", `transform 333ms`)
+  elm.classList.add("drag-over")
+ console.clear()
+  console.log();
+   const isDraggedHalfElement = (isPrevElementDraggedOver ? (draggingElmRect.top <= rect.bottom - rect.height / 2) : (draggingElmRect.bottom >= rect.top + rect.height / 2) )
+   if (isDraggedHalfElement){
+     this.#changeDraggingElementVisualIndex(isPrevElementDraggedOver ? this.prevState : this.nextState)
+     document.onpointermove = null
     }
-  }
 
-  #changeDraggingElementVisualIndex({ elm: dragOverElement, rect: dragOverElmRect}) {
-    const oldTransformValue = /\d+/.exec(dragOverElement.style.getPropertyValue("transform"))
-    const oldTranslateY = (oldTransformValue ? parseInt(oldTransformValue[0]) : 0)
-    const dragOverElementVisualIndex = parseInt(dragOverElement.getAttribute("data-visual-index")) 
-    dragOverElement.style.setProperty("transform", `translateY(${this.draggingState.startRect.top - dragOverElmRect.top - (this.draggingState.visualIndex - dragOverElementVisualIndex === 1 ? oldTranslateY : -oldTranslateY)}px)`)
-    dragOverElement.setAttribute("data-visual-index", this.draggingState.visualIndex)
-    this.draggingState.startRect = dragOverElmRect
-    this.#updateState(null, dragOverElementVisualIndex)
-  }
+    setTimeout(() => {
+      document.onpointermove = this.#dragMoveHandler.bind(this)
 
+    },333)
+  // const isPrevElementDraggedOver = (this.prevState.rect && draggingElmRect.top <= this.prevState.rect.top + this.prevState.rect.height / 2)
+  // const isNextElementDraggedOver = (this.nextState.rect && draggingElmRect.top >= this.nextState.rect.top - this.nextState.rect.height / 2)
+  // if (isPrevElementDraggedOver || isNextElementDraggedOver)
+  //   this.#changeDraggingElementVisualIndex(isPrevElementDraggedOver ? this.prevState : this.nextState)
+ }
+ #dragLeaveHandler(e) {
+ }
   #dragEndHandler() {
     const { elm } = this.draggingState;
     if (elm) {
